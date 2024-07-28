a52bbb9929bfd326a08e973befe2bd59c22da571
ronit <ronitkumar9874@gmail.com>
2024-07-28 20:12:16 +0530

    Added script location

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,13 @@ require("@openzeppelin/hardhat-upgrades");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  paths: {
+    sources: "./Contracts",
+    tests: "./test",
+    cache: "./cache",
+    scripts: "./scripts",
+    artifacts: "./artifacts"
+  },
   networks: {
     hardhat: {
       chainId: 1337
